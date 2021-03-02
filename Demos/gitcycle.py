9b3273413ef9a849e2d2de9479b8b0c26127e23f
repl.it user <replituser@example.com>
2021-03-02 10:03:50 +0000

    Week 1 demos complete

---
+++ Demos/gitcycle.py
@@ -0,0 +1,4 @@
+print ("hello")
+print ("First step: git add .")
+print ("Second step: git commit -m \"your message\"")
+print ("Last step: git push")
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # COM411-Problem-Solving-through-Programming1
+
 Week 1 inputs/outputs 
+
+Week 2
