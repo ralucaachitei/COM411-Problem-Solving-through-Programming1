37f085db69cfec1e5144ae55d7f77aa64d68ea14
repl.it user <replituser@example.com>
2021-03-02 11:11:34 +0000

    if statements

--- Demos/gitcycle.py
+++ Demos/gitcycle.py
@@ -2,3 +2,13 @@ print ("hello")
 print ("First step: git add .")
 print ("Second step: git commit -m \"your message\"")
 print ("Last step: git push")
+
+print("What is your name?")
+#Variable is a constainer which can store data for us in the memory(string, integer, float, boal)
+name = input()
+print("What is your age?")
+age = int(input())
+print("what is your bank balance?")
+balance = float(input())
+
+print("Welcome {}. You said to be {} years old. Your bank balance is {}.". format(name, age, balance))
