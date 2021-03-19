2fd700531e3284ef18d1982b9b0b5166090fc9dd
repl.it user <replituser@example.com>
2021-03-19 09:58:04 +0000

    added code to demonstrate a simple list

--- Demos/week4/list.py
+++ Demos/week4/list.py
@@ -26,3 +26,23 @@ print(fruits)
 
 #Access single element
 print(fruits[0])
+
+
+def get_fruits():
+  fruits = []
+  print("How many fruits would you like to enter?")
+  n = int(input())
+  for i in range(n):
+    print("type in the next fruit:")
+    fruits.append(input())
+
+  #Print all items
+  print("Your fruits are {}".format(fruits))
+  #Print few items by Slicing
+  print(f"Sliced list: {fruits[0:5]}")
+
+  #Print only few items using negative index
+  print(f"Negative index: {fruits[-2:0:-1]}")
+
+
+get_fruits()
