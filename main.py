9f59ec3fcb06697b7eeff886e66dd76925de958b
repl.it user <replituser@example.com>
2021-04-01 10:11:24 +0000

    Shopping

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
 def shop():
   items = {
-    "eggs": 1.99,
-    "milk": 0.99,
-    "cereals": 2.99,
-    "steak": 4.79,
+    "Eggs": 1.99,
+    "Milk": 0.99,
+    "Cereals": 2.99,
+    "Steak": 4.79,
     "Beer": 2.99,
     "Sausage": 1.29,
     "Vinegar": 2.49,
     "Bread": 1.49
-     }
+    }
   return items
+
 
 def view_all(products ={}):
   for i in products:
     print(i)
+
+def basket():
+  basket = []
+  while True:
+    item = input("Enter item or (\"stop\"): ")
+    if item == "stop":
+      break
+    else: 
+      basket.append(item)
+  return basket
+
+def till(basket =[]):
+  shoplist = shop()
+  total = 0.0
+  for item in basket:
+    total += shoplist[item]
+  return total
+
+def run():
+ print("welcome to Raluca's shop! Please have a look around and add item")
+ chosen_items = basket()
+ while True:
+   print("are you ready to pay?")
+   if input().lower()== "yes":
+     to_pay = till(chosen_items)
+     print(f"please pay ${to_pay:.2f} by cash or card")
+     break
+   else:
+     chosen_items += basket
+
+
+run()
+
+
+
